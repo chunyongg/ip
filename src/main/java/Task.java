84fff5b6af56a2f584f8b8236a06279247eb2a01
Lim Chun Yong <lim.chunyong@U.nus.edu>
2020-08-17 02:34:02 +0800

    lvl2

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,23 +1,11 @@
 public class Task {
     String taskName;
-    boolean isCompleted;
     Task(String task) {
         this.taskName = task;
-        this.isCompleted = false;
-    }
-
-    private Task(String task, boolean isCompleted) {
-        this.taskName = task;
-        this.isCompleted = isCompleted;
-    }
-
-    public Task setTaskAsCompleted() {
-        return new Task(taskName, true);
     }
 
     @Override
     public String toString() {
-        String tickOrCross  = isCompleted? "\u2713" : "\u2717";
-        return String.format("%s %s %s %s", "[", tickOrCross, "] ", taskName);
+        return taskName;
     }
 }
